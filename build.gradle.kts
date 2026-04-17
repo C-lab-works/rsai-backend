cc32ff0b0404bb7fc9216ee8d7d435468b894a54
Tatsuya Tanaka <github@tatsut.jp>
2026-04-17 13:52:02 +0900

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,21 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "dev.gate"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
